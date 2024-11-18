5d730daf121a952c048f6a4cc150e45d56a99cd4
Fan Yang <yangfan879@gmail.com>
2024-11-18 22:34:41 +0800

    recognize invalid rollback

--- pgserver/logrepl/replication_test.go
+++ pgserver/logrepl/replication_test.go
@@ -30,7 +30,6 @@ import (
 	"github.com/cockroachdb/errors"
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/jackc/pgx/v5"
-	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -505,7 +504,7 @@ var replicationTests = []ReplicationTest{
 }
 
 func TestReplication(t *testing.T) {
-	logrus.SetLevel(logrus.DebugLevel)
+	// logrus.SetLevel(logrus.DebugLevel)
 	RunReplicationScripts(t, replicationTests)
 }
 
@@ -545,14 +544,14 @@ func RunReplicationScripts(t *testing.T, scripts []ReplicationTest) {
 	require.NoError(t, logrepl.CreatePublication(primaryDns, slotName))
 	time.Sleep(500 * time.Millisecond)
 
-	for i, script := range scripts {
-		if i == 9 {
-			RunReplicationScript(t, dsn, script)
-		}
-	}
-	// for _, script := range scripts {
-	// 	RunReplicationScript(t, dsn, script)
+	// for i, script := range scripts {
+	// 	if i == 9 {
+	// 		RunReplicationScript(t, dsn, script)
+	// 	}
 	// }
+	for _, script := range scripts {
+		RunReplicationScript(t, dsn, script)
+	}
 }
 
 const slotName = "myduck_slot"
