10e5a17f7d85a8eb5cae64d62bf44631df2ec916
Sean Wu <111744549+VWagen1989@users.noreply.github.com>
2024-12-03 16:16:09 +0800

    fix: correct the 'retrieved_gtid_set' by adopting the 'pendingPosition'

--- binlogreplication/binlog_replica_controller.go
+++ binlogreplication/binlog_replica_controller.go
@@ -382,8 +382,8 @@ func (d *myBinlogReplicaController) GetReplicaStatus(ctx *sql.Context) (*binlogr
 	copy.ReplicateIgnoreTables = d.filters.getIgnoreTables()
 
 	if !d.applier.currentPosition.IsZero() {
+		copy.RetrievedGtidSet = d.applier.pendingPosition.GTIDSet.String()
 		copy.ExecutedGtidSet = d.applier.currentPosition.GTIDSet.String()
-		copy.RetrievedGtidSet = copy.ExecutedGtidSet
 	}
 
 	changeSourceLogFileToInvalidIfEmpty(&copy)
