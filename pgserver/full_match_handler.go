2369f13f1ff0f2753f908ccda54d211390f8d74c
TianyuZhang1214 <neo@apecloud.com>
2024-12-27 11:46:42 +0800

    adopt PR

--- pgserver/full_match_handler.go
+++ pgserver/full_match_handler.go
@@ -2,6 +2,7 @@ package pgserver
 
 import (
 	"regexp"
+	"strings"
 )
 
 // queryPatterns maps regular expressions to SQL queries to handle PostgreSQL-specific queries
@@ -23,16 +24,32 @@ import (
 // Since handling all such cases is complex, we only handle a limited set of common queries,
 // especially those frequently used with PostgreSQL clients.
 
-var queryPatterns = map[*regexp.Regexp]string{
-	regexp.MustCompile(`(?is)^SELECT\s+pg_type\.oid,\s*enumlabel\s+FROM\s+pg_enum\s+JOIN\s+pg_type\s+ON\s+pg_type\.oid=enumtypid\s+ORDER\s+BY\s+oid,\s+enumsortorder$`): "SELECT pg_type.oid, pg_enum.enumlabel FROM pg_enum JOIN pg_type ON pg_type.oid=enumtypid ORDER BY pg_type.oid, pg_enum.enumsortorder;",
+var pattern = regexp.MustCompile(`[ \n\r\t]+`)
+
+type HardCodedQuery struct {
+	original  string
+	converted string
+}
+
+var HardCodedQueries = struct {
+	SelectPgTypeOid HardCodedQuery
+}{
+	SelectPgTypeOid: HardCodedQuery{
+		original:  "SELECT pg_type.oid, enumlabel FROM pg_enum JOIN pg_type ON pg_type.oid=enumtypid ORDER BY oid, enumsortorder",
+		converted: "SELECT pg_type.oid, pg_enum.enumlabel FROM pg_enum JOIN pg_type ON pg_type.oid=enumtypid ORDER BY pg_type.oid, pg_enum.enumsortorder;",
+	},
+}
+
+var queryPatterns = map[string]string{
+	preProcessing(HardCodedQueries.SelectPgTypeOid.original): HardCodedQueries.SelectPgTypeOid.converted,
 }
 
 // handleFullMatchQuery checks if the given query matches any known patterns and returns the corresponding SQL query.
 func handleFullMatchQuery(inputQuery string) string {
-	for pattern, sql := range queryPatterns {
-		if pattern.MatchString(inputQuery) {
-			return sql
-		}
-	}
-	return ""
+	return queryPatterns[preProcessing(inputQuery)]
+}
+
+func preProcessing(inputQuery string) string {
+	trimmed := pattern.ReplaceAllString(inputQuery, "")
+	return strings.ToLower(trimmed)
 }
