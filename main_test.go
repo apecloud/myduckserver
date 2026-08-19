8e70ba7600fa8eff2dcfd46b16c48d5cd308791b
leon-ape <leon@apecloud.com>
2026-08-20 02:48:05 +0800

    fix: translate MySQL FIND_IN_SET to DuckDB list_position (#369)
    FIND_IN_SET(str, csv) becomes COALESCE(LIST_POSITION(STRING_SPLIT(csv, ','), str), 0)
    so missing items return 0 like MySQL.
    
    Co-authored-by: leon-ape <256035575+leon-ape@users.noreply.github.com>

--- main_test.go
+++ main_test.go
@@ -343,9 +343,7 @@ func TestQueriesSimple(t *testing.T) {
 		"show_function_status_like_'foo'",
 		"show_function_status_where_Db='mydb'",
 		"SELECT_CONV(i,_10,_2)_FROM_mytable",
-		"select_find_in_set('second_row',_s)_from_mytable;",
-		"select_find_in_set(s,_'first_row,second_row,third_row')_from_mytable;",
-		"select_i_from_mytable_where_find_in_set(s,_'first_row,second_row,third_row')_=_2;",
+
 		"____SELECT_COUNT(*)__FROM_keyless__WHERE_keyless.c0_IN_(_____WITH_RECURSIVE_cte(depth,_i,_j)_AS_(_______SELECT_0,_T1.c0,_T1.c1_______FROM_keyless_T1_______WHERE_T1.c0_=_0_________UNION_ALL_________SELECT_cte.depth_+_1,_cte.i,_T2.c1_+_1_______FROM_cte,_keyless_T2_______WHERE_cte.depth_=_T2.c0___)_____SELECT_U0.c0___FROM_keyless_U0,_cte___WHERE_cte.j_=_keyless.c0____)_____ORDER_BY_c0;_",
 		"____SELECT_COUNT(*)__FROM_keyless__WHERE_keyless.c0_IN_(_____WITH_RECURSIVE_cte(depth,_i,_j)_AS_(_______SELECT_0,_T1.c0,_T1.c1_______FROM_keyless_T1_______WHERE_T1.c0_=_0_________UNION_ALL_________SELECT_cte.depth_+_1,_cte.i,_T2.c1_+_1_______FROM_cte,_keyless_T2_______WHERE_cte.depth_=_T2.c0___)_____SELECT_U0.c0___FROM_cte,_keyless_U0____WHERE_cte.j_=_keyless.c0_____)_____ORDER_BY_c0;_",
 		"_select_*_from_mytable,__lateral_(__with_recursive_cte(a)_as_(___select_y_from_xy___union___select_x_from_cte___join___(____select_*_____from_xy____where_x_=_1____)_sqa1___on_x_=_a___limit_3___)__select_*_from_cte_)_sqa2_where_i_=_a_order_by_i;",
